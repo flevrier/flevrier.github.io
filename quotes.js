808ea85fa40e2555dd56725db81168b7ccd7aedf
François Levrier <flevrier@free.fr>
2026-03-07 10:15:08 +0100

    Built site for gh-pages

---
+++ quotes.js
@@ -0,0 +1,11 @@
+fetch("quotes.txt")
+.then(response => response.text())
+.then(text => {
+
+let quotes = text.split("\n\n");
+
+let q = quotes[Math.floor(Math.random()*quotes.length)];
+
+document.getElementById("quote").innerHTML = q;
+
+});
